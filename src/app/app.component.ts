70b91ff7ced0bc7eb3026e0b43639265a599cece
kelvinrtobias <kelvinrtobias@gmail.com>
2024-09-02 20:12:04 -0400

    first Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //app-root is within index.html
+  selector: 'app-root', //the HTML tag you can use to render thie component
+  templateUrl: './app.component.html', // <-- clean data
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AppointmentApp';
